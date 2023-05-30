01a919877366f51f693aa6879fa332246a2cedb0
usmanyousuf98 <usmanyousuf02@gmail.com>
2023-05-29 21:54:36 -1200

    console logs cleared

--- src/index.js
+++ src/index.js
@@ -26,7 +26,6 @@ const app = express();
 app.use(cors());
 
 app.use((req, res, next) => {
-  console.log("Okay ", req.headers.authorization);
   const { method, path } = req;
   console.log(
     `New request to: ${method} ${path} at ${new Date().toISOString()}`
